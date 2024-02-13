665b4e67323b1cdb2220ab87985ac1c9b9db5973
Rodrigo Bergamaschi <rodrigobergamaschi@gmail.com>
2024-02-13 11:35:37 -0300

    Criada pesquisa por ano

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import express from 'express';
 import historicoInflacao from './dados/historicoInflacao.js'
-import buscaIpcaPorID from './servicos/funcoes.js'
+import buscaIpcaPorID, { filtraPorAno } from './servicos/funcoes.js'
 const app = express();
 
-app.get('/historicoInflacao', (req, res) => {
-    res.json(historicoInflacao)
-  });
 
-  app.get('/historicoInflacao/:id', (req, res) =>{
-   let idParametro = parseInt(req.params.id);
+app.get('/historicoIPCA', (req, res) =>{
+  let Ano = parseInt(req.query.ano)
+  let resultado = Ano ? filtraPorAno(Ano) : historicoInflacao;
+  if(resultado.length > 0)
+  {
+    res.json(resultado)
+  }
+  else{
+    res.status(404).send({"Erro":"Nenhum resultado válido."})
+  }
+
+ 
+})
+
+app.get('/historicoIPCA/:id', (req, res) =>{
+    let idParametro = parseInt(req.params.id);
    
     let id = buscaIpcaPorID(idParametro)
     res.json(id)
--- servicos/funcoes.js
+++ servicos/funcoes.js
@@ -5,5 +5,11 @@ export function buscaIpcaPorID (id)
    let idEncontrado =  historicoInflacao.find(item => item.id === id)
    return idEncontrado;
 }
+
+export function filtraPorAno(ano)
+{
+   let Ano = historicoInflacao.filter(item => item.ano === ano)
+   return Ano;
+}
     
 export default buscaIpcaPorID;
