dc2f1871f526438c3184966a78a25259c5f17183
Rodrigo Bergamaschi <rodrigobergamaschi@gmail.com>
2024-02-12 23:13:57 -0300

    retornando colecao inteira

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-var teste
+import express from 'express';
+import historicoInflacao from './dados/historicoInflacao.js'
+const app = express();
+
+app.get('/historicoInflacao', (req, res) => {
+    res.json(historicoInflacao)
+  });
+
+
+
+app.listen(8080, () => {
+    console.log('Servidor iniciado na porta 8080');
+  });
---
+++ servicos/funcoes.js
@@ -0,0 +1,6 @@
+import historicoInflacao from '../dados/historicoInflacao.js';
+
+export const buscaIpca =() =>
+{
+    return historicoInflacao;
+}
